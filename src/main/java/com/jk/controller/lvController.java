7b3f113d3d0fcca135acf14106332397a1c02421
jiagy <752045494@qq.com>
2019-05-15 20:04:41 +0800

    jgy 19:59 submit

--- src/main/java/com/jk/controller/lvController.java
+++ src/main/java/com/jk/controller/lvController.java
@@ -3,9 +3,15 @@ package com.jk.controller;
 import com.jk.service.LvService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class lvController {
     @Autowired
     private LvService lvService;
+
+    @RequestMapping("find")
+    public String  find(String url){
+        return url;
+    }
 }
